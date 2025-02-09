ea11df026868101c883eda60052067a89cc9b38f
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2025-02-09 23:05:34 +0530

    implemented mongoDB connection and routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const taskRoutes = require("./src/routes/taskRoutes");
 
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/tasks", taskRoutes);
+
+// Connect to mongooDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB Successfully");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((error) => console.error(error));
