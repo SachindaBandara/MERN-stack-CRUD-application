f54f55bff913c5d0f7dc0e0db2bd6a75c1e09f11
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2025-02-09 23:40:16 +0530

    Created API such as get all tasks and add new task

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,3 +1,50 @@
+const express = require("express");
+const router = express.Router();
+const Task = require("../models/Task");
+
+//======================
+//  Get all tasks
+//======================
+
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//======================
+//  Get a single task by ID
+//======================
+
+route.get("/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found!" });
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//======================
+//  Post create a new task
+//======================
+
+route.post("/", async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(404).json({ message: error.message });
+  }
+});
 
 
+//======================
+//  PUT/PATCH update an existing task
+//======================
 
