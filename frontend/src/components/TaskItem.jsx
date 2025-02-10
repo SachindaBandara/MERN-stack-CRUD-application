c335c07b8c09658d8550f4db16beffa7f112fd37
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2025-02-10 20:54:09 +0530

    add

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -4,16 +4,16 @@ const TaskItem = ({ task, handleEdit, handleDelete }) => {
   return (
     <li className='border p-2 rounded mb-2 flex justify-between items-center'>
       <div>
-        <h2 className='font-bold'>Task Title</h2>
-        <p>Task description</p>
+        <h2 className='font-bold'>{task.title}</h2>
+        <p>{task.description}</p>
       </div>
 
       <div>
-        <button className='bg-yellow-500 text-white p-1 rounded mr-2'>
+        <button onClick={() => handleEdit(task)} className='bg-yellow-500 text-white p-1 rounded mr-2'>
           Edit
         </button>
 
-        <button className='bg-red-500 text-white p-1 rounded'>
+        <button onClick={() => handleDelete(task._id)} className='bg-red-500 text-white p-1 rounded'>
           Delete
         </button>
       </div>
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div>
+    <ul>
       
-    </div>
+    </ul>
   )
 }
 
